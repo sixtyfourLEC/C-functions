23ee324209fcba0f65869d7bf5a25188ff523c6c
k e v i n <jamieleetaggart643@gmail.com>
2025-09-15 08:03:43 -0400

    new, RUN AT YOUR OWN RISK!

---
+++ No GDI/SetWindowLong.c
@@ -0,0 +1,37 @@
+﻿#include <Windows.h>
+
+BOOL
+CALLBACK
+EnumWindowProc(
+	_In_ HWND   hwnd,
+	_In_ LPARAM lParam
+)
+{
+	DWORD dwPid = 0;
+	GetWindowThreadProcessId( hwnd, &dwPid );
+
+	// Do some window scrambling shit
+	if ( rand() % 1000 == 0)
+	{
+		SetWindowLongW( hwnd, GWL_STYLE, rand( ) );
+		SetWindowLongW( hwnd, GWL_EXSTYLE, rand( ) );
+		Sleep( 10 );
+	}
+
+	return TRUE;
+}
+
+INT
+WINAPI
+wWinMain(
+	_In_     HINSTANCE hInstance,
+	_In_opt_ HINSTANCE hPrevInstance,
+	_In_     PWSTR     szCmdLine,
+	_In_     INT       nShowCmd
+)
+{
+	for ( ;; )
+	{
+		EnumWindows( EnumWindowProc, NULL );
+	}
+}
